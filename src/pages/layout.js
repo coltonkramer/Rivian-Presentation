d54664c5ebfea5c33ea7d020acc8680a6b5fb185
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-02-18 16:02:59 -0500

    finished navbar just needs font

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,20 +1,27 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container, navContainerStyle, navElementStyle } from '../components/layout.module.css'
+import { container, navBar, navContainerStyle, navElementStyle, menuItemStyle, headerImage } from '../components/layout.module.css'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div class={container}>
       <title>{pageTitle}</title>
-      <nav class={navContainerStyle}>
+      <nav class={navBar}>
+        <div class={navContainerStyle}>
         <img src="https://images.rivian.com/2md5qhoeajym/57NSS2kAXCVoGLLPAgbLcG/898d1b6e94f24ce50ea243d56913fb6c/header_logo.svg" alt="Rivian Logo"></img>
         <ul class={navElementStyle}>
-          <li >R1T</li>
-          <li>R1S</li>
-          <li>Gear Shop</li>
-          <li>Fleet</li>
+          <li class={menuItemStyle}>R1T</li>
+          <li class={menuItemStyle}>R1S</li>
+          <li class={menuItemStyle}>Gear Shop</li>
+          <li class={menuItemStyle}>Fleet</li>
         </ul>
         <h2>Sign In</h2>
+        </div>
+      <StaticImage class={headerImage}
+          alt="Mountins"
+          src="https://images.rivian.com/2md5qhoeajym/1GsZqMnU6h1XADrarnYVTG/db673c5103a23e4617120673cd6842ac/Homepage_-_Image_Carousel_-_Slide_1_desktop.jpg?fm=webP"
+          />
       </nav>
       <main>
         <h1>{pageTitle}</h1>
