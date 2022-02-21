b5a93079c0606e2a97f12ba4264d3e93861138ba
coltonkramer <77127051+coltonkramer@users.noreply.github.com>
2022-02-21 10:13:45 -0500

    moved nav element where should be, added header

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,28 +1,30 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { container, navBar, navContainerStyle, navElementStyle, menuItemStyle, headerImage } from '../components/layout.module.css'
+import { container, headerContainer, navContainerStyle, navElementStyle, menuItemStyle, headerImage } from '../components/layout.module.css'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div class={container}>
       <title>{pageTitle}</title>
-      <nav class={navBar}>
+      <header class={headerContainer}>
       <StaticImage class={headerImage}
           alt="Mountins"
           src="https://images.rivian.com/2md5qhoeajym/1GsZqMnU6h1XADrarnYVTG/db673c5103a23e4617120673cd6842ac/Homepage_-_Image_Carousel_-_Slide_1_desktop.jpg?fm=webP"
           />
         <div class={navContainerStyle}>
         <img src="https://images.rivian.com/2md5qhoeajym/57NSS2kAXCVoGLLPAgbLcG/898d1b6e94f24ce50ea243d56913fb6c/header_logo.svg" alt="Rivian Logo"></img>
+        <nav>
         <ul class={navElementStyle}>
           <li class={menuItemStyle}>R1T</li>
           <li class={menuItemStyle}>R1S</li>
           <li class={menuItemStyle}>Gear Shop</li>
           <li class={menuItemStyle}>Fleet</li>
         </ul>
+        </nav>
         <h2>Sign In</h2>
         </div>
-      </nav>
+      </header>
       <main>
         <h1>{pageTitle}</h1>
         {children}
